2d0fe9e99ae4fd5153aaa0e27e0c67ea97ab5590
Joost Kuif <joost.kuif@bytesinmotion.nl>
2024-09-26 13:33:37 +0200

    add debug messages, bug fix

--- custom_components/homeassistant-bitvavo/sensor.py
+++ custom_components/homeassistant-bitvavo/sensor.py
@@ -1,11 +1,17 @@
+import logging
 from homeassistant.helpers.entity import Entity
 from python_bitvavo_api.bitvavo import Bitvavo
+
+# Set up logging
+#logging.basicConfig(level=logging.DEBUG)
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up Bitvavo sensor platform."""
     name = config_entry.data["name"]
     apikey = config_entry.data["apikey"]
     apisecret = config_entry.data["apisecret"]
+    _LOGGER.debug("Setting up the custom sensor platform")
 
     # Initialize the Bitvavo client with user-supplied API key and secret
     bitvavo_client = Bitvavo({
@@ -62,12 +68,14 @@ class BitvavoPriceSensor(Entity):
 
     def update(self):
         """Fetch the latest price from Bitvavo API."""
-        try:            
+        _LOGGER.debug("Updating the state of the custom sensor")
+        try:
             prices = {}
             # Fetch the balance and prices of all coins
             balances = self._client.balance() # Fetch the balance of all your coins (this is where your API key and secret are used)
             tickers = self._client.tickerPrice() #Fetch the current price of all markets. (public data)
-            print(f"tickers: {tickers}")
+            _LOGGER.debug(f"balances: {balances}")
+            _LOGGER.debug(f"tickers: {tickers}")
 
             # Cache the prices of all markets in a key/value array for faster lookups
             for item in tickers:
@@ -84,7 +92,8 @@ class BitvavoPriceSensor(Entity):
                 if symbol == 'EUR-EUR':
                     current_price = 1.0
                 else:
-                    current_price = prices.get(symbol, {'price': 0.0})  # add the market to the list if it doesn't exist.
+                    current_price = prices.get(symbol, 0.0)
+                _LOGGER.debug(f"symbol {symbol} -> current price: {current_price}")
 
                 # Calculate the total value of the coin
                 coin_balance = float(balance['available'])
@@ -98,11 +107,11 @@ class BitvavoPriceSensor(Entity):
                 # Accumulate the total balance
                 total_balance += total_value
 
-                print(f"{balance['symbol']} - Balance: {coin_balance} - Price: {current_price} - Value: {total_value}")
+                _LOGGER.debug(f"{balance['symbol']} - Balance: {coin_balance} - Price: {current_price} - Value: {total_value}")
 
-            print(f"\nTotal Balance in EUR: {total_balance}")
+            _LOGGER.debug(f"\nTotal Balance in EUR: {total_balance}")
             self._price = round(float(total_balance), 2)
-            
-        except Exception as err:
+
+        except Exception:
             self._price = None
-            print(f"Error fetching data from Bitvavo: {err}")
+            _LOGGER.exception("Error fetching data from Bitvavo")
